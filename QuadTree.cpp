5cd4d0c1e7dfb708d2136f4d97df0f191cbf5d47
Aiden Williams <aiden.williams.19@um.edu.mt>
2020-11-24 14:02:19 +0100

    Attempted using the insert function however, will use a different method from what is explained on geeksforgeeks

--- QuadTree.cpp
+++ QuadTree.cpp
@@ -10,14 +10,51 @@ void Quad::insert(Node *node){
 
     // Current quad cannot contain it
     if (!inBoundary(node->pos)) return;
-
     // We are at a quad of unit area
     // We cannot subdivide this quad further
+    // topLeft X
+    // Y       botRight
     if (abs(topLeft.x - botRight.x) <= 1 && abs(topLeft.y - botRight.y) <= 1){
         if (n == nullptr) n = node;
         return;
     }
 
+    if ((topLeft.x + botRight.x) / 2 >= node->pos.x){
+        // Indicates topLeftTree
+        if ((topLeft.y + botRight.y) / 2 >= node->pos.y){
+            if (topLeftTree == nullptr){
+                topLeftTree = new Quad(Point(topLeft.x, topLeft.y),Point((topLeft.x + botRight.x) / 2,
+                                                                         (topLeft.y + botRight.y) / 2));
+            }
+            topLeftTree->insert(node);
+        }else{
+            if (botLeftTree == nullptr){
+                botLeftTree = new Quad(Point(topLeft.x,(topLeft.y + botRight.y) / 2),
+                                       Point((topLeft.x + botRight.x) / 2, botRight.y));
+            }
+            botLeftTree->insert(node);
+        }
+    }else{
+        // Indicates topRightTree
+        if ((topLeft.y + botRight.y) / 2 >= node->pos.y){
+            if (topRightTree == nullptr){
+                topRightTree = new Quad(Point((topLeft.x + botRight.x) / 2, topLeft.y),Point(botRight.x,
+                                                                                             (topLeft.y + botRight.y) / 2));
+            }
+
+            topRightTree->insert(node);
+        }else{
+            if (botRightTree == nullptr) {
+                botRightTree = new Quad(Point((topLeft.x + botRight.x) / 2, (topLeft.y + botRight.y) / 2),
+                                        Point(botRight.x, botRight.y));
+            }
+            botRightTree->insert(node);
+        }
+    }
+}
+
+/*
+ * //now to see where to put node
     if ((topLeft.x + botRight.x) / 2 >= node->pos.x){
         // Indicates topLeftTree
         if ((topLeft.y + botRight.y) / 2 >= node->pos.y){
@@ -50,7 +87,7 @@ void Quad::insert(Node *node){
             botRightTree->insert(node);
         }
     }
-}
+ */
 
 // Find a node in a quadtree
 Node* Quad::search(Point p){
--- main.cpp
+++ main.cpp
@@ -17,11 +17,12 @@ bool powerof4(unsigned n){
 
 int main(int argc, char* args[]) {
     std::vector<Node> readFile;
-    auto it = readFile.begin();
+    //std::vector<Quad> quadTree;
     std::fstream input;
     bool isTxt;
     std::string line;
-    int width = 0, height = 0;
+    size_t width = 0, height = 0;
+    int x = 0, y = 0;
 
     if(argc != 2){
         std::cout << "Program requires argument for file to open!" << std::endl;
@@ -55,19 +56,50 @@ int main(int argc, char* args[]) {
             exit(EXIT_FAILURE);
         }
 
-        if(!powerof4(width)){
+        if(!powerof4(width)) {
             std::cout << "Program input width and height must conform to x^4" << std::endl;
             exit(EXIT_FAILURE);
         }
 
-        Quad *quadtree = new Quad(readFile.front().pos, readFile.back().pos);
-        quadtree->insert(&readFile.front());
+        Quad *quadTree = new Quad(readFile.front().pos, readFile.back().pos);
 
-        while(width != 1){
+        for(int i = 0; i < width; i+=2){
+//            quadTree.emplace_back(Quad(readFile.at(i).pos,readFile.at(i + 1 + width).pos));
+//
+//            quadTree.back().insert(&readFile.at(i));
+//            quadTree.back().insert(&readFile.at(i + 1));
+//            quadTree.back().insert(&readFile.at(i + 2));
+//            quadTree.back().insert(&readFile.at(i + 3));
 
-            width /= 2;
-            height /= 2;
+            quadTree->insert(&readFile.at(i));
+            quadTree->insert(&readFile.at(i + 1));
+            quadTree->insert(&readFile.at(i + 2));
+            quadTree->insert(&readFile.at(i + 3));
+
+            if(i == width - 1){
+                x += width;
+                i = 0;
+            }
         }
+
+        free(quadTree);
+
+//        Quad *quadtree = new Quad(readFile.front().pos, readFile.back().pos);
+//
+//        //top left
+//        quadtree->insert(&readFile.at(x));
+//        //bottom left
+//        quadtree->insert(&readFile.at(x + width));
+//        //top right
+//        quadtree->insert(&readFile.at(y));
+//        //bottom right
+//        quadtree->insert(&readFile.at(y + height));
+//
+//        while(width != 1){
+//
+//            width /= 2;
+//            height /= 2;
+//        }
 
 
     }else{
